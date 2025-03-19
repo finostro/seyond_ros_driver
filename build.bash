4ba94986d8d820d0c69b2025e0ea3fe4a64777b6
Yuechao.Gu <yuechao.gu@cn.seyond.com>
2025-03-19 11:24:21 +0800

    [chore]: update build script

--- build.bash
+++ build.bash
@@ -5,13 +5,16 @@ set -e
 CURRENT_PATH=$(cd $(dirname $0); pwd)
 CLIENT_SDK_PATH=${CURRENT_PATH}/src/seyond_lidar_ros/src/seyond_sdk/
 
+echo -e "\n\033[1;32m-- (a). Clean the old build files...\033[0m"
 rm -rf ${CURRENT_PATH}/build ${CURRENT_PATH}/install ${CURRENT_PATH}/devel ${CURRENT_PATH}/log
 rm -rf ${CURRENT_PATH}/src/CMakeLists.txt
 
+echo -e "\n\033[1;32m-- (b). Check the Seyond SDK...\033[0m"
 if [[ -e "${CLIENT_SDK_PATH}/lib/libinnolidarsdkclient.so" ]]; then
   echo "The inno_sdk has already been compiled, but please verify it is compatible with the current platform"
 else
   # build cilent sdk
+  echo "The inno_sdk has not been compiled, start to compile the inno_sdk"
   cd ${CLIENT_SDK_PATH}/build
   shared=1 ./build_unix.sh
   echo "build status $?"
@@ -19,7 +22,7 @@ else
 fi
 
 
-
+echo -e "\n\033[1;32m-- (c). Build the driver...\033[0m"
 if [[ "$ROS_VERSION" -eq 1 ]]; then
   catkin_make install
 elif [[ "$ROS_VERSION" -eq 2 ]]; then
@@ -27,3 +30,4 @@ elif [[ "$ROS_VERSION" -eq 2 ]]; then
 else
     echo "Can't find ROS_VERSION or ROS2_VERSION"
 fi
+echo -e "\n\033[1;32m-- (d). Build Done!\033[0m"
--- create_deb.bash
+++ create_deb.bash
@@ -5,17 +5,36 @@ if [ -z "$ROS_VERSION" ] || [ -z "$ROS_DISTRO" ]; then
   exit 1
 fi
 
-echo "ROS_VERSION: $ROS_VERSION"
-echo "ROS_DISTRO: $ROS_DISTRO"
-
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 PROJECT_DIR=${CURRENT_DIR}
 DEB_ROS_ROOT=opt/ros/${ROS_DISTRO}
 
+echo -e "\n\033[1;32m-- (1). Check the build result...\033[0m"
+# check the driver has been built 
+if [[ -e "${PROJECT_DIR}/install/setup.sh" ]]; then
+  echo "The driver has been built"
+  source ${PROJECT_DIR}/install/setup.sh
+  echo "ROS_VERSION: $ROS_VERSION"
+  echo "ROS_DISTRO: $ROS_DISTRO"
+  # update the DISTRO
+  DEB_ROS_ROOT=opt/ros/${ROS_DISTRO}
+else
+  # build the driver
+  echo "The driver has not been built, start to build the driver"
+  echo "ROS_VERSION: $ROS_VERSION"
+  echo "ROS_DISTRO: $ROS_DISTRO"
+  ${PROJECT_DIR}/build.bash
+fi
+
+echo -e "\n\033[1;32m-- (2). Clean the output directory...\033[0m"
+echo "The output directory is ${CURRENT_DIR}/output"
 # result output
 OUTPUT_DIR=${CURRENT_DIR}/output
+rm -rf ${OUTPUT_DIR}
 mkdir -p ${OUTPUT_DIR}
 cd ${OUTPUT_DIR}
+
+echo -e "\n\033[1;32m-- (3). Build the deb package...\033[0m"
 
 # build in local
 DEB_VERSION=1.0.0
@@ -55,3 +74,5 @@ rm -rf deb_root
 rm -rf ${CURRENT_DIR}/deb_control
 ls -lrt
 popd
+
+echo -e "\n\033[1;32m-- (4). Done!\033[0m"
