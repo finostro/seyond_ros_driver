32d459cc75c6aa302a48a2787a0649c031842987
Yuechao.Gu <yuechao.gu@cn.seyond.com>
2025-03-31 11:01:08 +0800

    [chore]: static link seyond-sdk

--- build.bash
+++ build.bash
@@ -10,13 +10,13 @@ rm -rf ${CURRENT_PATH}/build ${CURRENT_PATH}/install ${CURRENT_PATH}/devel ${CUR
 rm -rf ${CURRENT_PATH}/src/CMakeLists.txt
 
 echo -e "\n\033[1;32m-- (b). Check the Seyond SDK...\033[0m"
-if [[ -e "${CLIENT_SDK_PATH}/lib/libinnolidarsdkclient.so" ]]; then
-  echo "The inno_sdk has already been compiled, but please verify it is compatible with the current platform"
+if [[ -e "${CLIENT_SDK_PATH}/lib/libinnolidarsdkclient.a" ]]; then
+  echo "The seyond_sdk has already been compiled, but please verify it is compatible with the current platform"
 else
   # build cilent sdk
-  echo "The inno_sdk has not been compiled, start to compile the inno_sdk"
+  echo "The seyond_sdk has not been compiled, start to compile the seyond_sdk"
   cd ${CLIENT_SDK_PATH}/build
-  shared=1 ./build_unix.sh
+  ./build_unix.sh
   echo "build status $?"
   cd -
 fi
